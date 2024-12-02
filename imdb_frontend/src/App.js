de73d83c80d20091575f1acadd26a71618671332
Maria-Lavinia Otelea <mlo@frankly.dk>
2024-12-02 21:57:35 +0100

    WIP: Nav+Footer comps

--- imdb_frontend/src/App.js
+++ imdb_frontend/src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route,  Routes } from 'react-router';
+import Navigation from './components/navigation/Navigation';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import Footer from './components/footer/Footer';
 function App() {
   return (
    <BrowserRouter>
    
    <Routes>
-      <Route path="/" element={<div>elem</div>} />
+      <Route path="/" element={<div>
+        <Navigation />
+        <Footer />
+      </div>} />
       <Route path="/login" element={<div>elem 1</div>} />
       <Route path="/register" element={<div>elem 2</div>} />     
    </Routes>
---
+++ imdb_frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+
+const Footer = ()   => {
+    return (
+        <Container fluid style={{ backgroundColor: '#21252b', position: 'fixed', bottom: 0, width: '100%' }} className='justify-content-center align-items-center'>
+            <footer className="footer" style={{color: "white", textAlign:"center"}}>
+                <p>© 2024 IMDb</p>
+            </footer>
+        </Container>
+    );
+}
+
+export default Footer;
