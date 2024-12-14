edb39219848056b36a19a75ccc83aa73d8f607fc
Adelina Stefania Radulescu <adelina.radulescu8@yahoo.com>
2024-12-14 20:30:53 +0100

    fixed rating movie

--- imdb_frontend/src/services/Rating/ratingService.js
+++ imdb_frontend/src/services/Rating/ratingService.js
@@ -1,25 +1,21 @@
-const saveRating = async (userId, tconst, ratingValue) => {
-  try {
-    const response = await fetch(
-      `${process.env.REACT_APP_BASE_URL}/api/rating/move/${userId}/${tconst}/${ratingValue}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${localStorage.getItem('token')}`,
-        },
-      }
-    );
+const saveRating = async (userId, tconst, ratingValue, token) => {
+	try {
+		const response = await fetch(
+			`${process.env.REACT_APP_BASE_URL}/api/rating/movie/${userId}/${tconst}/${ratingValue}`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
 
-    if (!response.ok) {
-      return response.status;
-    }
-
-    return response.ok;
-  } catch (error) {
-    console.error('There was a problem saving the rating.', error);
-    throw error;
-  }
+		return response.status;
+	} catch (error) {
+		console.error('There was a problem saving the rating.', error);
+		throw error;
+	}
 };
 
 export { saveRating };
